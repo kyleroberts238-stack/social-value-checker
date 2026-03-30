be57dd48f206fdf1b7da1f0469183e1ffe0c8f7d
kyleroberts238-stack <kyleroberts238@gmail.com>
2026-03-30 15:07:22 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,18 +20,16 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-
         {children}
-
       </body>
     </html>
   );
